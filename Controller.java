21d8cebdb366fb2f6a81fdeb3ef138f1b2aa05cb
Jakub Tomas <tomasjakub21@gmail.com>
2020-01-27 12:21:36 +0100

    first commit

---
+++ Controller.java
@@ -0,0 +1,49 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+
+    public ComboBox<String> comboBox;
+
+    List countries;
+    List cities;
+
+    public Controller() throws Exception {
+       /* Database database = new Database();
+        database.getCountries();*/
+    }
+
+
+    public void showCountry() {
+        Database database = new Database();
+        countries = database.getCountries();
+        comboBox.getItems().setAll(countries);
+    }
+
+    public void showData(ActionEvent actionEvent) {
+        String countries = "SELECT country.name FROM country limit 1";
+        PreparedStatement stmt = null;
+        try {
+            stmt = Database.getConnection().prepareStatement(countries);
+            ResultSet rs = stmt.executeQuery();
+            String country;
+            List<String> list = new ArrayList<>();
+            while (rs.next()) {
+                country = rs.getString("Name");
+                System.out.println(country);
+                list.add(rs.getString("Name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ Database.java
@@ -0,0 +1,88 @@
+package sample;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+
+    private static final String JDBC = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://itsovy.sk:3306/world_x?autoReconnect=true&useSSL=false";
+    //  private static final String URL = "jdbc:mysql://itsovy.sk:3306/chat1n?autoReconnect=true&useSSL=false";
+    // jdbc:mysql://localhost:3306/world_x?autoReconnect=true&useSSL=false
+    private static Connection connection;
+
+
+    public static Connection getConnection() throws Exception, SQLException {
+        Class.forName(JDBC);
+        connection = DriverManager.getConnection(URL, "student", "kosice2019");
+        return connection;
+    }
+
+    public List getListOfCountries() {
+        try {
+            Connection con = getConnection();
+            PreparedStatement preparedStatement = con.prepareStatement("select  * from country limit 10 ");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString(1);
+                String a = resultSet.getString(2);
+                String b = resultSet.getString(3);
+                String c = resultSet.getString(4);
+                // String d = resultSet.getString(5);
+                System.out.println(name  + "  " + a  + "  "+ b + " " + "  " +  c +"  " );
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+
+    /*public List getCountries() throws Exception {
+        String countries="SELECT country.name FROM country limit 1";
+        PreparedStatement stmt=getConnection().prepareStatement(countries);
+        ResultSet rs=stmt.executeQuery();
+        String country;
+        List<String> list=new ArrayList<>();
+        while (rs.next()){
+            country=rs.getString("Name");
+            System.out.println(country);
+            list.add(rs.getString("Name"));
+        }
+
+        return null;
+
+    }*/
+
+    public List getCountries() {
+        try {
+            Connection connection = getConnection();
+            List<String> country = new ArrayList<>();
+            String select = "Select  name from country";
+
+            PreparedStatement statement = connection.prepareStatement(select);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                country.add(resultSet.getString(1));
+            }
+
+
+            return country;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+}
